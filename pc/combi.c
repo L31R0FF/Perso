6ac6a6471fd66da192a51c8b9bd0b33310ab2dc3
L31R0FF <l31r0ff@laposte.net>
2017-10-22 13:01:34 +0200

    Update combi.c

--- pc/combi.c
+++ pc/combi.c
@@ -3,12 +3,13 @@
 
 int main(int argc, char *argv[]) {
   char list[LISTNB] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+  char phon[LISTNB][8] = {"a","bé","cé","dé","é","eff","gé","ach","i","ji","ca","elle","eme","ene","o","pé","cu","erre","esse","té","u","vé","double v","ixe","i grec","zed"};
   int i, i2, i3;
   
   for (i = 0; i < LISTNB; i++) {
     for (i2 = 0; i2 < LISTNB; i2++) {
       for (i3 = 0; i3 < LISTNB; i3++) {
-        printf("%c%c%c\n", list[i],list[i2],list[i3]);
+        printf("%c%c%c - %s%s%s\n", list[i],list[i2],list[i3],phon[i],phon[i2],phon[i3]);
       }
     }  
   }
